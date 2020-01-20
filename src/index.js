f6cef89f102091d6ae938deaf329a31317229cd1
Jaid <jaid.jsx@gmail.com>
2020-01-20 02:44:56 +0100

    Upgraded dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 /** @module react-modern-picture */
 
-import React from "react"
 import classnames from "classnames"
+import {isNil, isString} from "lodash"
 import PropTypes from "prop-types"
-import {isString,isNil} from "lodash"
+import React from "react"
 
 /**
   * @typedef {{
@@ -27,15 +27,15 @@ export default class extends React.Component {
       PropTypes.arrayOf(PropTypes.string),
       PropTypes.arrayOf(PropTypes.object),
     ]),
-    input: PropTypes.any.isRequired
+    input: PropTypes.any.isRequired,
   }
 
   render() {
     const classProps = {}
-    if (this.props.className){
+    if (this.props.className) {
       classProps = classnames(this.props.className)
     }
-    if (isString(this.props.input )) {
+    if (isString(this.props.input)) {
       return <img src={this.props.input} {...classProps}/>
     }
     if (isNil(this.props.input.webp) && isString(this.props.input.fallback)) {
