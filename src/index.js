8f727f07209a649bf89caad7fa7071d5456b6f52
Jaid <jaid.jsx@gmail.com>
2020-01-20 02:18:15 +0100

    Added base
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,23 +1,48 @@
 /** @module react-modern-picture */
 
+import React from "react"
+import classnames from "classnames"
+import PropTypes from "prop-types"
+import {isString,isNil} from "lodash"
+
 /**
- * @function
- * @param {number} [compareValue]
- * @returns {number} Seconds passed since Unix epoch (01 January 1970)
- * @example
- * import reactModernPicture from "react-modern-picture"
- * const result = reactModernPicture()
- * result === 1549410770
- * setTimeout(() => {
- *   const result2 = reactModernPicture(result)
- *   result2 === 3
- * }, 3000)
+  * @typedef {{
+  *   className: *,
+  *   input: *,
+  * }} Props
+  */
+
+/**
+ * @class
+ * @extends {React.Component<Props>}
  */
-export default compareValue => {
-  const seconds = Math.floor(Date.now() / 1000)
-  if (compareValue === undefined) {
-    return seconds
-  } else {
-    return seconds - compareValue
+export default class extends React.Component {
+
+  static displayName = _PKG_NAME
+
+  static propTypes = {
+    className: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object,
+      PropTypes.arrayOf(PropTypes.string),
+      PropTypes.arrayOf(PropTypes.object),
+    ]),
+    input: PropTypes.any.isRequired
   }
+
+  render() {
+    const classProps = {}
+    if (this.props.className){
+      classProps = classnames(this.props.className)
+    }
+    if (isString(this.props.input )) {
+      return <img src={this.props.input} {...classProps}/>
+    }
+    if (isNil(this.props.input.webp) && isString(this.props.input.fallback)) {
+      return <picture>
+        {content}
+      </picture>
+    }
+  }
+
 }
--- test/test.js
+++ test/test.js
@@ -1,13 +1,19 @@
 import path from "path"
+import React from "react"
+import reactTestRenderer from "react-test-renderer"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 
 /**
  * @type { import("../src") }
  */
-const {default: reactModernPicture} = indexModule
+const {default: ReactModernPicture} = indexModule
+
 
 it("should run", () => {
-  const result = reactModernPicture()
-  expect(result).toBeGreaterThan(1549410770)
+  const src = "https://i.imgur.com/5NfvQ7y.jpg"
+  const dom = <ReactModernPicture input={src}/>
+  const result = reactTestRenderer.create(dom).toJSON()
+  expect(result.type).toStrictEqual("img")
+  expect(result.props.src).toStrictEqual(src)
 })
